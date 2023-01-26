573c5ccf3b3cc7cfeb180726616fbbd4659081e1
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-01-26 18:31:38 +0000

    rename, attempt blur

--- src/Desktop.js
+++ src/Desktop.js
@@ -1,10 +1,10 @@
-import './App.css';
+import './Desktop.css';
 
 function App() {
   return (
     <div id='container'>
       <div id='wallpaper'>
-        <img src="https://cdn.wallpaperhub.app/cloudcache/b/c/a/f/b/0/bcafb0bcbd097fb78dc37ba5c755efe51dbd204e.jpg" alt='10x wallpaper' className='wallpaper-image'/>
+        <img src="https://marketplace.canva.com/EAFHm4JWsu8/1/0/1600w/canva-pink-landscape-desktop-wallpaper-HGxdJA_xIx0.jpg" alt='10x wallpaper' className='wallpaper-image'/>
       </div>
       <div id='bottom'>
         <div id='bar'>
