b8cadc0878c53f883005429bf631df6752b6abdf
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-01-26 14:46:32 +0000

    start work on 10x desktop

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div id='container'>
+      <div id='wallpaper'>
+        <img src="https://t4.ftcdn.net/jpg/05/21/18/03/360_F_521180377_2iAVJqBQSo3cgKaVp8vMBR8asrC61DoU.jpg" alt='10x wallpaper' className='wallpaper-image'/>
+      </div>
+      <div id='bottom'>
+      </div>
     </div>
   );
 }
