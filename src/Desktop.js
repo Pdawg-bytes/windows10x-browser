3ff9cf60c2638e9f6dd8e9fc1e2e23fea3fa42be
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-01-27 14:10:09 +0000

    why doesnt this work

--- src/Desktop.js
+++ src/Desktop.js
@@ -4,15 +4,13 @@ function App() {
   return (
     <div id='container'>
       <div id='wallpaper'>
-        <img src="https://marketplace.canva.com/EAFHm4JWsu8/1/0/1600w/canva-pink-landscape-desktop-wallpaper-HGxdJA_xIx0.jpg" alt='10x wallpaper' className='wallpaper-image'/>
+      <img src="https://marketplace.canva.com/EAFHm4JWsu8/1/0/1600w/canva-pink-landscape-desktop-wallpaper-HGxdJA_xIx0.jpg" alt='10x wallpaper' className='wallpaper-image'/>
       </div>
-      <div id='bottom'>
-        <div id='bar'>
+      <div id='bar'>
 
-        </div>
-        <div id='icons'>
+      </div>
+      <div id='icons'>
 
-        </div>
       </div>
     </div>
   );
