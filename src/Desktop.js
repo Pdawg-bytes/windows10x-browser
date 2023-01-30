27a4acbd0d1a4ef4ea92580b4d235811f9aa2e5b
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-01-30 14:10:24 +0000

    rework for blur

--- src/Desktop.js
+++ src/Desktop.js
@@ -2,17 +2,14 @@ import './Desktop.css';
 
 function App() {
   return (
-    <div id='container'>
-      <div id='wallpaper'>
-      <img src="https://marketplace.canva.com/EAFHm4JWsu8/1/0/1600w/canva-pink-landscape-desktop-wallpaper-HGxdJA_xIx0.jpg" alt='10x wallpaper' className='wallpaper-image'/>
-      </div>
-      <div id='bar'>
-
-      </div>
-      <div id='icons'>
-
-      </div>
+  <div class="effects">
+    <div class="effect-holder dark">
+        <div class="dropdown-style black">
+            <img src="" alt=""/>
+            <span>Some Message</span>
+        </div>
     </div>
+  </div>
   );
 }
 
