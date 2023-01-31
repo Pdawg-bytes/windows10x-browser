40cd2b458feb4bd8b51d51fb91682bc2e9288153
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-01-31 17:10:15 +0000

    work on clock

--- src/Desktop.js
+++ src/Desktop.js
@@ -1,11 +1,41 @@
 import './Desktop.css';
 
+function showTime() {
+  // Init clock variables
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var session = "AM";
+
+  if (h === 0) {
+      h = 12;
+  }
+
+  if (h > 12) {
+      h = h - 12;
+      session = "PM";
+  }
+
+  h = (h < 10) ? h : h;
+  m = (m < 10) ? "0" + m : m;
+
+  var time = h + ":" + m;
+  document.getElementById("clockRight").innerText = time;
+  document.getElementById("clockRight").textContent = time;
+
+  // Recall func every second
+  setTimeout(showTime, 1000);
+
+}
+
+// showTime();
+
 function App() {
   return (
-  <div class="container">
+  <div class="container" onload="currentTime()">
     <div class="desktop">
       <div class="bar">
-        
+        <p class="clockRight">4:20</p>
       </div>
     </div>
   </div>
