dfbe582064a84a90978613c4ce9c0e1c0e352e21
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-02-02 14:10:11 +0000

    add fluent icons

--- src/desktop.js
+++ src/desktop.js
@@ -26,28 +26,31 @@ function currentTime() {
 }
 window.addEventListener('load', currentTime);
 
-function flyoutHoverTimer()
+function batteryGet()
 {
-
+  let isBatterySupported = 'getBattery' in navigator;
+  if(!isBatterySupported) {
+     
+  }
 }
-
+batteryGet();
 function App() {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
   <div class="container" onload="currentTime()" tabindex="-1">
     <div class="desktop">
-      <div className={`actionTooltip ${isHovered ? 'visible' : 'hidden'}`}>Action Center</div>
-      <img src="https://github.com/Futur3Sn0w/Windows10x/blob/main/Walls/wall1.jpg?raw=true" className='wallpaper' alt='wallpaper'/>
-      <div class="bar">
-        <div class="statusDiv">
+      <div tabindex="-1" className={`actionTooltip ${isHovered ? 'visible' : 'hidden'}`}>Action Center</div>
+      <img tabindex="-1" src="https://github.com/Futur3Sn0w/Windows10x/blob/main/Walls/wall1.jpg?raw=true" className='wallpaper' alt='wallpaper'/>
+      <div class="bar" tabindex="-1">
+        <div class="statusDiv" tabindex="-1">
           <button class="statusButton" tabindex="-1" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-            <span class="clock">
-              <p class="clockText">0:00</p>
+            <span class="clock" tabindex="-1">
+              <p class="clockText" tabindex="-1">0:00</p>
             </span> 
-            <div class="statusIcons">
-              <span class="wifiIcon">&#xE701;</span>
-              <span class="batteryIcon">&#xE83F;</span>
+            <div class="statusIcons" tabindex="-1">
+              <span class="wifiIcon" tabindex="-1">&#xE701;</span>
+              <span class="batteryIcon" tabindex="-1">&#xE83F;</span>
             </div>    
           </button>
         </div>
